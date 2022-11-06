8d237270d7970259ac9986a485e3d5ac5c1db0cf
Natfan <nat@natfan.io>
2022-11-06 20:31:49 +0000

    Added base index file

---
+++ index.js
@@ -0,0 +1,11 @@
+require("dotenv").config()
+const { Client, Events, GatewayIntentBits } = require(~discord.js~)
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`)
+})
+
+client.login(process.env.DISCORD_TOKEN)
